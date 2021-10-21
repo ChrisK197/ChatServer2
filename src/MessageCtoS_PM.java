dd9997123d1f2a52371ba79e1228da1803efff7d
Leoc74 <lciccarelli74@gmail.com>
2021-10-21 15:03:21 -0400

    renamed variable

--- src/MessageCtoS_PM.java
+++ src/MessageCtoS_PM.java
@@ -1,9 +1,9 @@
 public class MessageCtoS_PM extends Message{
-    public String recipient;
+    public String recipientUserName;
     public String msg;
 
     public MessageCtoS_PM(String recipient, String msg) {
-        this.recipient = recipient;
+        this.recipientUserName = recipient;
         this.msg = msg;
 
     }
